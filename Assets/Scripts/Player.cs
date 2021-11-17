6b6e126b07fab3cfc54d95e88c69f3f578e8efec
jayypluss <jonathanfk1@gmail.com>
2021-11-17 18:02:56 -0300

    Adding damage and life hearts.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,15 @@ public class Player : MonoBehaviour
     public float jumpLength;
     public float jumpHeight;
     public float slideLength;
+    public float minSpeed = 10f;
+    public float maxSpeed = 30f;
+    public float invincibleTime;
+    public int maxLife = 3;
+
+    // For blinking using other shaders that don't have _BlinkingValue *
+    // * Uncomment next line
+    // public GameObject model;
+    // * Pass "CatMesh" object to "Model" Player's property (from Unity interface) 
 
 
     private Animator anim;
@@ -16,17 +25,22 @@ public class Player : MonoBehaviour
     private BoxCollider boxCollider;
 
     private int currentLane = 1;
+    private int currentLife;
+    static int blinkingValue;
     private float jumpStart;
     private float slideStart;
 
     private bool isJumping = false;
     private bool isSliding = false;
     private bool isSwiping = false;
+    private bool invincible = false;
 
 
     private Vector3 verticalTargetPosition;
     private Vector3 boxColliderSize;
     private Vector2  startingTouch;
+
+    private UIManager  uiManager;
 
     // Start is called before the first frame update
     void Start()
@@ -36,11 +50,37 @@ public class Player : MonoBehaviour
         boxCollider = GetComponent<BoxCollider>();
         boxColliderSize = boxCollider.size;
         anim.Play("runStart");
+        currentLife = maxLife;
+        speed = minSpeed;
+        blinkingValue = Shader.PropertyToID("_BlinkingValue");
+        uiManager = FindObjectOfType<UIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (isJumping)
+        {
+            float ratio = (transform.position.z - jumpStart) / jumpLength;
+            if (ratio >= 1f)
+            {
+                isJumping = false;
+                anim.SetBool("Jumping", false);
+                anim.Play("runStart");
+            }
+            else
+            {
+                verticalTargetPosition.y = Mathf.Sin(ratio * Mathf.PI) * jumpHeight;
+            }
+        }
+        else
+        {
+            verticalTargetPosition.y = Mathf.MoveTowards(verticalTargetPosition.y, 0, 5 * Time.deltaTime);
+        }
+
+
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             ChangeLane(-1);
@@ -102,7 +142,6 @@ public class Player : MonoBehaviour
             {
                 isSwiping = false;
             }
-
         }
 
         if (Input.touchCount == 1)
@@ -141,6 +180,16 @@ public class Player : MonoBehaviour
             }
         }
 
+        if (isSliding)
+        {
+            float ratio = (transform.position.z - slideStart) / slideLength;
+            if (ratio >= 1f) {
+                isSliding = false;
+                anim.SetBool("Sliding", false);
+                boxCollider.size = boxColliderSize;
+            }
+        }
+
         if (Input.GetTouch(0).phase == TouchPhase.Began)
         {
             startingTouch = Input.GetTouch(0).position;
@@ -151,34 +200,6 @@ public class Player : MonoBehaviour
             isSwiping = false;
         }
 
-
-        if (isJumping)
-        {
-            float ratio = (transform.position.z - jumpStart) / jumpLength;
-            if (ratio >= 1f)
-            {
-                isJumping = false;
-                anim.SetBool("Jumping", false);
-            }
-            else
-            {
-                verticalTargetPosition.y = Mathf.Sin(ratio * Mathf.PI) * jumpHeight;
-            }
-        }
-        else
-        {
-            verticalTargetPosition.y = Mathf.MoveTowards(verticalTargetPosition.y, 0, 5 * Time.deltaTime);
-        }
-
-        if (isSliding)
-        {
-            float ratio = (transform.position.z - slideStart) / slideLength;
-            if (ratio >= 1f) {
-                isSliding = false;
-                anim.SetBool("Sliding", false);
-                boxCollider.size = boxColliderSize;
-            }
-        }
 
         Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, laneSpeed * Time.deltaTime);
@@ -221,5 +242,66 @@ public class Player : MonoBehaviour
             boxCollider.size = newSize;
             isSliding = true;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (invincible)
+            return;
+             
+        if (other.CompareTag("Obstacle"))
+        {
+            currentLife--;
+            uiManager.UpdateLives(currentLife);
+            anim.SetTrigger("Hit");
+            speed = 0;
+            if (currentLife <= 0)
+            {
+                // Game Over
+            }
+            else
+            {
+                StartCoroutine(Blinking(invincibleTime));
+            }
+        }
+    }
+
+    IEnumerator Blinking(float time)
+    {
+        invincible = true;
+        float timer = 0;
+        float currentBlink = 1f;
+        float lastBlink = 0;
+        float blinkPeriod = 0.1f;
+
+        // * Comment this line in
+        // bool enabled = false;
+
+        yield return new WaitForSeconds(1f);
+        speed = minSpeed;
+        while (timer < time && invincible)
+        {
+            // * Uncomment next line
+            // LightmapsModeLegacy.SetActive(enabled);
+
+            // * Comment next line out
+             Shader.SetGlobalFloat(blinkingValue, currentBlink);
+             yield return null;
+             timer += Time.deltaTime;
+             lastBlink += Time.deltaTime;
+             if (blinkPeriod < lastBlink)
+             {
+                 lastBlink = 0;
+                 currentBlink = 1f - currentBlink;
+                // * Uncomment next line
+                //  enabled = !enabled;
+             }
+        }
+        // * Uncomment next line
+        // model.SetActive(true);
+
+        // * Comment next line out
+        Shader.SetGlobalFloat(blinkingValue, 0);
+        invincible = false;
     }
 }
